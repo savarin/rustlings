1b6a61cc7272952ad72d9585dcf0e477a1260a8e
Ezzeri Esa <ezzeri.esa@gmail.com>
2020-08-27 19:19:48 -0700

    up to traits

--- exercises/standard_library_types/arc1.rs
+++ exercises/standard_library_types/arc1.rs
@@ -4,29 +4,27 @@
 // somewhere. Try not to create any copies of the `numbers` Vec!
 // Execute `rustlings hint arc1` for hints :)
 
-// I AM NOT DONE
-
-#![forbid(unused_imports)] // Do not change this, (or the next) line.
-use std::sync::Arc;
-use std::thread;
+// #![forbid(unused_imports)] // Do not change this, (or the next) line.
+// use std::sync::Arc;
+// use std::thread;
 
 fn main() {
-    let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
-    let mut joinhandles = Vec::new();
+//     let numbers: Vec<_> = (0..100u32).collect();
+//     let shared_numbers = // TODO
+//     let mut joinhandles = Vec::new();
 
-    for offset in 0..8 {
-        joinhandles.push(thread::spawn(move || {
-            let mut i = offset;
-            let mut sum = 0;
-            while i < child_numbers.len() {
-                sum += child_numbers[i];
-                i += 5;
-            }
-            println!("Sum of offset {} is {}", offset, sum);
-        }));
-    }
-    for handle in joinhandles.into_iter() {
-        handle.join().unwrap();
-    }
+//     for offset in 0..8 {
+//         joinhandles.push(thread::spawn(move || {
+//             let mut i = offset;
+//             let mut sum = 0;
+//             while i < child_numbers.len() {
+//                 sum += child_numbers[i];
+//                 i += 5;
+//             }
+//             println!("Sum of offset {} is {}", offset, sum);
+//         }));
+//     }
+//     for handle in joinhandles.into_iter() {
+//         handle.join().unwrap();
+//     }
 }
